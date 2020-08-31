77c3f39609922a8070a80209a7c5dcd6ea617514
Alexa Gamil <alexis_gamil99@yahoo.com>
2020-08-31 18:40:55 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -5,3 +5,13 @@ def square_array(array)
   end
   return new_array 
 end
+
+def square_array(array)
+  i = 0 
+  new_array = []
+  while i < array.length do
+    new_array.push(array[i]*array[i])
+    i += 1 
+  end
+  return new_array 
+end
